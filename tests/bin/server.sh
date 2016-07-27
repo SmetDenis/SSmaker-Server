97d19cf0ef664159f413548df71ef8a641904e22
smetdenis <admin@jbzoo.com>
2016-07-27 12:34:02 +0700

    commands fot Makefiles

--- tests/bin/server.sh
+++ tests/bin/server.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/env bash
+#!/usr/bin/env sh
+
+#
+# JBZoo SSmaker-Server
+#
+# This file is part of the JBZoo CCK package.
+# For the full copyright and license information, please view the LICENSE
+# file that was distributed with this source code.
+#
+# @package   SSmaker-Server
+# @license   MIT
+# @copyright Copyright (C) JBZoo.com,  All rights reserved.
+# @link      https://github.com/JBZoo/SSmaker-Server
+#
 
 DIR="$( cd -P "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 WEB_HOST="127.0.0.1"
@@ -8,13 +21,4 @@ WEB_PATH="$WEB_ROOT/index.php"
 
 cd $DIR
 
-php -S "$WEB_HOST:$WEB_PORT" -t "$WEB_ROOT" "$WEB_PATH"&
-
-echo "Waiting until PHP webserver is ready on port $WEB_PORT"
-while [[ -z `curl -s "http://$WEB_HOST:$WEB_PORT" ` ]]
-do
-    echo -n "."
-    sleep 2s
-done
-
-echo "PHP webserver is up"
+php -S "$WEB_HOST:$WEB_PORT" -t "$WEB_ROOT" "$WEB_PATH" &
